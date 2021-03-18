ae25be3d633cb939c969766c8d6f54e797879efe
Daniel <69875036+Danieloliver11@users.noreply.github.com>
2021-03-18 12:38:08 -0300

    exercicio 18/03

--- blogPessoal/src/main/java/com/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/com/generation/blogPessoal/controller/PostagemController.java
@@ -3,10 +3,15 @@ package com.generation.blogPessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +24,7 @@ import com.generation.blogPessoal.repository.PostagemRepository;
 @CrossOrigin("*")
 public class PostagemController {
 	@Autowired
-	private PostagemRepository repository; //pega da interface
+	private PostagemRepository repository; //pega da interface esse PostagemRepository.
 	
 	@GetMapping					
 	public ResponseEntity<List<Postagem>> GetAll(){
@@ -37,4 +42,29 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>>GetByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(repository.findAllBytituloContainingIgnoreCase(titulo));
 	}
+	
+	//end point de postagem.
+												//"post" vai devolver uma postagem no ResponseEntity
+												//"Postagem" objeto e o nome dele sera "postagem". 
+	@PostMapping								//"@RequestBody" Para Recepcionar os valores/objetos que são passadas via body para nossa aplicação
+	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+		//para salvar os dados recebidos 
+	}
+	
+	//put atualização de dados 
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	//delete
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+	
+	
 }

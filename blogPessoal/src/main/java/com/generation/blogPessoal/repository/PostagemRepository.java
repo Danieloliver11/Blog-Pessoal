c9f522e102b4d13e36007974c966d9481e08109c
Daniel <69875036+Danieloliver11@users.noreply.github.com>
2021-03-19 14:52:34 -0300

    exercicio 3

--- blogPessoal/src/main/java/com/generation/blogPessoal/repository/PostagemRepository.java
+++ blogPessoal/src/main/java/com/generation/blogPessoal/repository/PostagemRepository.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Repository;
 import com.generation.blogPessoal.model.Postagem;
 
 
-@Repository
+@Repository													//mapea A Postagem do tipo long que é referente ao Id.
 public interface PostagemRepository extends JpaRepository<Postagem,Long> {
 	public List<Postagem> findAllBytituloContainingIgnoreCase (String titulo);
-}
+	// vai fazer uma lista de Postagem e o nome dessa lista vai ser :findAllBytituloContainingIgnoreCase.
+	// findAllBytituloContainingIgnoreCasede: de Acordo com o cliente digitar sera ignorado maiúsculo ou minusculo e usara como parâmetro é titulo.
+} 

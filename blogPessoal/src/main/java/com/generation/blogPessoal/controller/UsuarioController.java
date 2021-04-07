3929bd66cf30d191d5042288729eb8ef3c1c9530
Daniel <69875036+Danieloliver11@users.noreply.github.com>
2021-04-06 21:08:51 -0300

    securança finalizada

---
+++ blogPessoal/src/main/java/com/generation/blogPessoal/controller/UsuarioController.java
@@ -0,0 +1,39 @@
+package com.generation.blogPessoal.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogPessoal.Service.UsuarioService;
+import com.generation.blogPessoal.model.UserLogin;
+import com.generation.blogPessoal.model.Usuario;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*" , allowedHeaders ="*")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService usuarioService; //injeção de service
+
+	// metodo para logar 
+	@PostMapping("/logar")
+	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
+	
+		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()); // build serve para montar toda a body.
+	}
+	//metodo para cadastrar
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> post(@RequestBody Usuario usuario){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(usuarioService.CadastrarUsuario(usuario));
+	}
+}
